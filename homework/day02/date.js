1c499804357b5d29aa1df90d0932b1bac39e1b82
dahekim <kimdre88@naver.com>
2022-03-16 09:58:18 +0900

    homework_getToday

--- homework/day02/date.js
+++ homework/day02/date.js
@@ -1,11 +1,11 @@
 function getToday(){
-    const aaa = new Date()
-    const yyyy = aaa.getFullYear()
-    const mm = String(aaa.getMonth()+1).padStart(2,"0")
-    const dd = String(aaa.getDate()).padStart(2,"0")
-    const hh = String(aaa.getHours()).padStart(2,"0")
-    const mm1 = String(aaa.getMinutes()).padStart(2,"0")
-    const ss = String(aaa.getSeconds()).padStart(2,"0")
+    const today = new Date()
+    const yyyy = today.getFullYear()
+    const mm = String(today.getMonth()+1).padStart(2,"0")
+    const dd = String(today.getDate()).padStart(2,"0")
+    const hh = String(today.getHours()).padStart(2,"0")
+    const mm1 = String(today.getMinutes()).padStart(2,"0")
+    const ss = String(today.getSeconds()).padStart(2,"0")
 
     const TodayDate = `오늘은 ${yyyy}년 ${mm}월 ${dd}일 ${hh}:${mm1}:${ss} 입니다.`
     console.log(TodayDate)
