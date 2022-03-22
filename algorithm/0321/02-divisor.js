d2a561631df3e49f831bb5abcea6b7679efe72c5
dahekim <kimdre88@naver.com>
2022-03-22 18:09:23 +0900

    0321 algorithm selfStudy & Class

--- algorithm/0321/02-divisor.js
+++ algorithm/0321/02-divisor.js
@@ -15,3 +15,38 @@ function solution(n, a=0, b=0) {
     return n<=a/2?b:solution(n,a+1,b+=n%a?0:a);
 }
 */
+
+function solution(n){
+  let answer = 0
+  for( let i = 1 ;  i<=n ; i++ ){
+    if(n % i === 0 ){
+      answer += i
+    }
+  }
+  return answer
+}
+
+function solution(n){
+  let answer = n
+
+  for( let i = 1 ;  i<= n / 2 ; i++ ){
+    // 약수
+    // n을 2로 나눴을 때...? n이 3000 이하니까 1500까지만 돌 수도 있다고...?
+    if(n % i === 0 ){
+      answer += i
+    }
+  }
+  return answer
+}
+
+function solution(n){
+  const answer = new Array(n)
+                      .fill(1)
+                      .reduce( (acc ,cur, i) =>{
+                                            return n%(cur + i) === 0
+                                              ? acc + (cur + i)       //약수라면 
+                                              : acc                   //그냥 넘겨줌
+                                             } ,0)
+  
+  return answer
+}
