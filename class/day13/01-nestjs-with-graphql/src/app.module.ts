23546b7762c103fe8413002d19998606fd3c81e0
dahekim <kimdre88@naver.com>
2022-03-30 21:45:48 +0900

    0330 class

---
+++ class/day13/01-nestjs-with-graphql/src/app.module.ts
@@ -0,0 +1,22 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { BoardModule } from './apis/boards/boards.module';
+
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// 하위 api들을 모두 모아서 import 해주는 역할
+
+@Module({
+    imports: [
+        BoardModule,
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: 'src/commons/graphql/schema.gql',
+        }),
+    ],
+    // controllers: [AppController],
+    // providers: [AppService],
+})
+export class AppModule {}
---
+++ class/day13/03-nestjs-with-docker/src/app.module.ts
@@ -0,0 +1,33 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { BoardModule } from './apis/boards/boards.module.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Board } from './apis/boards/entities/board.entity.js';
+
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// 하위 api들을 모두 모아서 import 해주는 역할
+
+@Module({
+    imports: [
+        BoardModule,
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: 'src/commons/graphql/schema.gql',
+        }),
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'my-database',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'mydocker02',
+            entities: [Board],
+            synchronize: true,
+            logging: true,
+        }),
+    ],
+})
+export class AppModule {}
