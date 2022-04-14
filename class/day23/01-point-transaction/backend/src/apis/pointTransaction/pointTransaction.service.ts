aa1bd43f825362ef1adf68835219efe7c97848a4
dahekim <kimdre88@naver.com>
2022-04-14 19:20:10 +0900

    0414 BE Class

---
+++ class/day23/01-point-transaction/backend/src/apis/pointTransaction/pointTransaction.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { CurrentUser } from "src/commons/auth/gql-user.param";
+import { Repository } from "typeorm";
+import { User } from "../users/entities/user.entity";
+import { PointTransaction, POINT_TRANSACTION_STATUS_ENUM } from "./entities/pointTransaction.entity";
+
+
+@Injectable()
+export class PointTransactionService{
+    constructor(
+        @InjectRepository(PointTransaction)
+        private readonly pointTransactionRepository : Repository<PointTransaction>,
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ){}
+    
+    async create( { impUid, amount, currentUser } ){
+        // 1. pointTransaction 테이블의 거래기록 1줄을 생성, 
+        // (실제로 포인트가 올리가지 않은 상태)
+        const pointTransaction = await this.pointTransactionRepository.save({
+            impUid: impUid,
+            amount: amount,
+            user: currentUser,
+            status:POINT_TRANSACTION_STATUS_ENUM.PAYMENT,
+        })
+
+        // 2. 유저의 포인트 데이터 찾아와서 업데이트하기 
+        const user = await this.userRepository.findOne({ id: currentUser.id })
+        await this.userRepository.update(
+            {id: user.id}, // 조건
+            {point: user.point + amount} // 업데이트할 컬럼 값
+        )
+
+        // 3. 최종 결과를 프론트엔드에 돌려주기 
+        return pointTransaction
+
+    }
+}
---
+++ class/day23/01-point-transaction/backend/src/app.module.ts
@@ -0,0 +1,42 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { BoardModule } from './apis/boards/boards.module.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductCategoryModule } from './apis/productsCategory/productCategory.module.js';
+import { ProductModule } from './apis/products/product.module.js';
+import { UserModule } from './apis/users/user.module.js';
+import { AuthModule } from './apis/auth/auth.module.js';
+import { PointTransactionModule } from './apis/pointTransaction/pointTransaction.module.js';
+
+
+@Module({
+    imports: [
+        AuthModule,
+        BoardModule,
+        PointTransactionModule,
+        ProductCategoryModule,
+        ProductModule,
+        UserModule,
+
+
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: 'src/commons/graphql/schema.gql',
+            context: ({req, res}) => ({req,res}),
+        }),
+        
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'qwer1234',
+            database: 'myproject02',
+            entities: [__dirname+ '/apis/**/*.entity.*'],
+            synchronize: true,
+            logging: true,
+        }),
+    ],
+})
+export class AppModule {}
