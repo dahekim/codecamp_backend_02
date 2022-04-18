02d7a03c1e90fbd2a43361981d3299b3e101884d
dahekim <kimdre88@naver.com>
2022-04-18 20:17:34 +0900

    0418 hw

--- homework/main-project/backend/src/apis/transaction/transaction.service.ts
+++ homework/main-project/backend/src/apis/transaction/transaction.service.ts
@@ -2,7 +2,7 @@ import { ConflictException, Injectable, UnprocessableEntityException } from '@ne
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from '../users/entities/users.entity';
 
-import { Repository } from 'typeorm';
+import { Connection, Repository, QueryRunner } from 'typeorm';
 import {
   Transaction,
   TRANSACTION_STATUS_ENUM,
@@ -16,6 +16,8 @@ export class TransactionService {
     private readonly transactionRepository: Repository<Transaction>,
     @InjectRepository(Users)
     private readonly userRepository: Repository<Users>,
+
+    private readonly connection: Connection
   ) {}
 
     // 중복확인
@@ -29,13 +31,13 @@ export class TransactionService {
     // 취소건수 확인
     async isCanceled({ impUid }){
       const result1 = await this.transactionRepository.findOne({
-        where: { impUid:impUid },
+        where: { impUid : impUid },
       })
       const result2 = await this.transactionRepository.findOne({
-        where: { status: TRANSACTION_STATUS_ENUM.CANCEL },
+        where: { status : TRANSACTION_STATUS_ENUM.CANCEL },
       })
 
-      if( result1&&result2 ) throw new ConflictException("이미 취소된 결제 아이디입니다.")
+      if( result1 && result2 ) throw new ConflictException("이미 취소된 결제 아이디입니다.")
     }
 
     // 취소포인트 확인
@@ -57,40 +59,68 @@ export class TransactionService {
     }
 
     async cancel({impUid, amount, currentUser}){
-      const transaction = await this.create({
-        impUid, 
-        amount: -amount, 
-        currentUser,
-        status: TRANSACTION_STATUS_ENUM.CANCEL,
-      })
-      return transaction
+      const queryRunner = await this.connection.createQueryRunner()
+      await queryRunner.connect()
+
+      await queryRunner.startTransaction("SERIALIZABLE")
+      try {
+        const transaction = await this.create({
+          impUid, 
+          amount: -amount, 
+          currentUser,
+          status: TRANSACTION_STATUS_ENUM.CANCEL,
+        })
+        await queryRunner.manager.save(transaction)
+        await queryRunner.commitTransaction()
+        return transaction
+      } catch(error) {
+        await queryRunner.rollbackTransaction()
+      } finally {
+        await queryRunner.release()
+      }
+      
     }
-
-
 
     async create({ impUid,amount, currentUser, status, }) {
-      // 1. transaction 테이블의 거래기록 1줄을 생성,
-      // (db에 거래기록만 반영되고 실제로 포인트가 올리가지는 않은 상태)
-      const transaction = await this.transactionRepository.create({
-        impUid: impUid,
-        amount: amount,
-        user: currentUser.id_user,
-        status: TRANSACTION_STATUS_ENUM.PAYMENT,
-      });
-      await this.transactionRepository.save(transaction)
+      // queryRunner-MySQL 연결
+      const queryRunner = await this.connection.createQueryRunner()
+      await queryRunner.connect()
 
-      // 2. 유저의 포인트 데이터 찾아와서 업데이트하기
-      const user = await this.userRepository.findOne({
-        where: { id_user: currentUser.id_user },
-      });
-      await this.userRepository.update(
-        { id_user: user.id_user }, // 조건
-        { point: user.point + amount }, // 업데이트할 컬럼 값
-      );
+      // transaction 시작
+      await queryRunner.startTransaction("SERIALIZABLE")
 
-      // 3. 최종 결과를 프론트엔드에 돌려주기
-      return transaction;
+      try {
+        // 1. transaction 테이블의 거래기록 1줄을 생성,
+        // (db에 거래기록만 반영되고 실제로 포인트가 올리가지는 않은 상태)
+        const transaction = this.transactionRepository.create({
+          impUid,
+          amount,
+          user : currentUser.id_user,
+          status : TRANSACTION_STATUS_ENUM.PAYMENT,
+        });
+        await queryRunner.manager.save(transaction)
+
+        // 2. 유저의 포인트 데이터 찾아와서 업데이트하기
+        const user = await queryRunner.manager.findOne(
+          Users,
+          {
+            where : [ {id_user: currentUser.id_user} ]
+          , lock: { mode:"pessimistic_write"} } ,  
+        )
+        const updatedUser = await this.userRepository.create({
+          ...user,
+          point: user.point + amount
+        })
+        await queryRunner.manager.save(updatedUser)
+
+        await queryRunner.commitTransaction()
+        // 3. 최종 결과를 프론트엔드에 돌려주기
+        return transaction;
+
+      } catch(error) {
+        await queryRunner.rollbackTransaction()
+      } finally {
+        await queryRunner.release()
+      }
     }
-
-  
 }
