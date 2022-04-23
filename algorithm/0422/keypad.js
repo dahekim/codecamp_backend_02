b27710d098149bf6a08daa6f35fa87bfaa39ce9e
dahekim <kimdre88@naver.com>
2022-04-23 17:31:05 +0900

    ^ㅅ^

--- algorithm/0422/keypad.js
+++ algorithm/0422/keypad.js
@@ -59,3 +59,53 @@ function solution(numbers, hand) {
     }
     return answer
 }
+
+// reduce(), Object.entries(), 비구조할당을 사용한 풀이 
+const [ leftNumbers,rightNumbers ] =[ [ 1,4,7 ], [ 3,6,9 ] ] 
+
+function solution(numbers, hand) {
+    // 현재 두 엄지 손가락이 어떤 위치에 있는지
+    const current ={
+        left : 10 ,
+        right : 12
+    }
+    return numbers.reduce( (acc,cur) => {
+        // useFinger : 어떤 손가락으로 눌렀는지 최종 리턴값("L", "R")
+        // target : 왼손인지 오른손인지 ("left", "right")
+        // number : 최종적으로 누른 번호 키패드 (숫자, 0~9)
+        let [ useFinger, target, number] =  ["" , "" , 0 ]
+        
+        if (leftNumbers.includes( cur ) ){
+            [useFinger, target, number] = ["L", "left", cur]
+        } else if ( rightNumbers.includes(cur) ) {
+            [useFinger, target, number] = ["R", "right", cur]
+        } else {
+            const locationObj = Object.entries( current ).reduce( ( acc2, cur2) => {
+                const targetHand = cur2[0]
+                
+                cur = cur === 0 ? 11 : cur
+                let location = Math.abs(cur - cur2[1])
+                
+                if ( location >2 ) { //location >=3
+                    location = Math.trunc(location / 3 + location %3)
+                }
+                acc2[ targetHand ] = location
+                return acc2
+            }, {})
+            if ( locationObj["left"] === locationObj["right"] ){
+                [useFinger, target, number ] = [
+                    hand === "left" ? "L" : "R",
+                    hand === "left" ? hand : "right",
+                    cur
+                ]
+            } else if ( locationObj["left"] > locationObj["right"] ){
+                [useFinger, target, number ] = [ "R", "right", cur ]
+            } else {
+                [useFinger, target, number ] = [ "L", "left", cur ]
+            }
+        }
+        current[target] = number
+				// console.log( cur, current, useFinger )
+        return acc + useFinger
+    }, "")
+}
